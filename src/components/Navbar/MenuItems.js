34a41e01a838524eb54174ee7605a078d41f1ea8
Karan <sharma.karan27oct@gmail.com>
2022-03-10 19:56:34 +0530

    Navbar Created

---
+++ src/components/Navbar/MenuItems.js
@@ -0,0 +1,26 @@
+export const MenuItems = [{
+        title: 'Link1',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Link2',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Link3',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Link4',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Sign Up',
+        url: '#',
+        cName: 'nav-links-mobile'
+    }
+]
