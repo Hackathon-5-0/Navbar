ba307bcf73e34405fb9ad434c3f652a29cf7eda0
Karan <sharma.karan27oct@gmail.com>
2022-03-13 12:30:04 +0530

    changed the theme of the navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from "react";
 import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer"
 import "./App.css";
 
 function App() {
-    return <div className = "App" > <Navbar/> </div>;
+    return <div className = "App" >
+        <Navbar/>
+        
+        <Footer/>
+    </div>;
 }
 
 export default App;
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -14,7 +14,7 @@ class Navbar extends Component {
   render() {
     return (
       <nav className="NavbarItems">
-        <h1 className="navbar-logo"> Name </h1>
+        <h1 className="navbar-logo"> <span>N</span>ame </h1>
         <div className="menu-icon" onClick={this.handleClick}>
           <i className={this.state.clicked ? 'fas fa-times':'fas fa-bars'}></i>
         </div>
