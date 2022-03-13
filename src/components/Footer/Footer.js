a06878d96aa0894ef05dbb4501b5bf79d1e46a38
Karan <sharma.karan27oct@gmail.com>
2022-03-13 13:28:10 +0530

    footer ready

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,14 +3,28 @@ import './Footer.css'
 
 class Footer extends Component{
     render(){
-        return(
-            <footer className="FooterItems">
-                <h1 className="footer-title">Name</h1>
-                <div className="footer-seperator"></div>
-                <div className="footer-titles">
-                    
+        return( 
+            <div className="Footer">
+                <div className="footermain">
+                    <h1><span>N</span>ame</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur.</p>
+                    <div class="social-icon">
+                        <div class="social-item">
+                            <a href="#"><img src="https://img.icons8.com/bubbles/100/000000/facebook-new.png" /></a>
+                        </div>
+                        <div class="social-item">
+                            <a href="#"><img src="https://img.icons8.com/bubbles/100/000000/instagram-new.png" /></a>
+                        </div>
+                        <div class="social-item">
+                            <a href="#"><img src="https://img.icons8.com/bubbles/100/000000/twitter.png" /></a>
+                        </div>
+                        <div class="social-item">
+                            <a href="#"><img src="https://img.icons8.com/bubbles/100/000000/behance.png" /></a>
+                        </div>
+                    </div>
+                    <p>Copyright © 2022 Name. All rights reserved</p>
                 </div>
-            </footer>
+            </div>
         )
     }
 }
